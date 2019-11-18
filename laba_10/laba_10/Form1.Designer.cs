3c5c4a3923fd25a6e17c7d9d3e99681c118e963b
Mepkypuu <mihail.kurguz2016@ynadex.ru>
2019-11-18 23:58:47 +0300

    Хелпани плиз

---
+++ laba_10/laba_10/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace laba_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(189, 29);
+            this.progressBar1.Maximum = 50;
+            this.progressBar1.MaximumSize = new System.Drawing.Size(523, 43);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(287, 29);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(38, 32);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(84, 255);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(614, 162);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(203, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(216, 14);
+            this.button4.TabIndex = 3;
+            this.button4.Text = ".";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Yellow;
+            this.button3.Location = new System.Drawing.Point(203, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(216, 16);
+            this.button3.TabIndex = 3;
+            this.button3.Text = ".";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Blue;
+            this.button2.Location = new System.Drawing.Point(203, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 19);
+            this.button2.TabIndex = 4;
+            this.button2.Text = ".";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(203, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 15);
+            this.button1.TabIndex = 3;
+            this.button1.Text = ".";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(408, 35);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(76, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 29);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "00 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(131, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "20";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(7, 35);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(62, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "C4";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Control;
+            this.button5.Location = new System.Drawing.Point(589, 50);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 59);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "СТАРТ";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 457);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ laba_10/laba_10/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba_10
+{
+    public partial class Form1 : Form
+    {
+        int temp;
+        int index; // yellow - 1, blue - 2, green - 3, red - 4.
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Сапер";
+            progressBar1.Size = new Size(200, 50);
+            progressBar1.Maximum = 200;
+            progressBar1.Step = 10;
+            timer1.Interval = 500;
+            timer1.Enabled = false;
+            timer1.Tick += timer1_Tick;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    if (((Button)item).Text == ".")
+                        ((Button)item).Click += CommonBtn_Click;
+                }
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.PerformStep();
+            temp = Convert.ToInt32(label2.Text);
+            temp--;
+            if (temp == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Время вышло!");
+                this.Close();
+            }
+            if(temp >= 10)
+                label2.Text = (temp.ToString());
+            else
+            label2.Text = ("0" + temp.ToString());          
+           /* if(progressBar1.Value == progressBar1.Maximum)
+            {
+                MessageBox.Show("Бомба взорвалась");
+                timer1.Stop();
+                progressBar1.Value = progressBar1.Minimum;
+                return;
+            }*/
+        }
+
+        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+
+            label2.Text = "20";
+            groupBox1.Visible = true;
+            progressBar1.Value = progressBar1.Minimum;
+            if(numericUpDown1.Value == 1)
+            {
+                button3.Visible = true;
+                button2.Visible = false;
+                button1.Visible = false;
+                button4.Visible = false;
+                index = rand.Next(1, 1);
+            }
+            if(numericUpDown1.Value == 2)
+            {
+                button1.Visible = false;
+                button4.Visible = false;
+                button3.Visible = true;
+                button2.Visible = true;
+                index = rand.Next(1, 2);
+            }
+            if(numericUpDown1.Value == 3 )
+            {
+                button4.Visible = false;
+                button3.Visible = true;
+                button2.Visible = true;
+                button1.Visible = true;
+                index = rand.Next(1, 3);
+            }
+            if(numericUpDown1.Value == 4)
+            {
+                button3.Visible = true;
+                button2.Visible = true;
+                button1.Visible = true;
+                button4.Visible = true;
+                index = rand.Next(1, 4);
+            }
+            
+            timer1.Enabled = true;           
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+  
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+    
+           
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+     
+           
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+        
+           
+        }
+
+        private void CommonBtn_Click(object sender, EventArgs e)
+        {
+            
+            if(((Button)sender).BackColor == Color.Yellow )
+            {
+                timer1.Stop();
+                MessageBox.Show("Бомба разминирована! Победа");
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("Бомба взорвалась! Поражение");
+                groupBox1.Visible = false;
+            }
+
+        }
+
+       
+    }
+}
